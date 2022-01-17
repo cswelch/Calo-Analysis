e07bf23ea67d90bc3ae1e8431c815fa016176365
Cole Welch <colewelch151@gmail.com>
2022-01-17 14:48:26 -0800

    Retrieved and plotted (Theta, Phi) on 2D histogram. Still seems to be a problem with the data since the distribution is so spread out.

--- calo_analysis.py
+++ calo_analysis.py
@@ -1,4 +1,6 @@
+from operator import indexOf
 import numpy as np
+from matplotlib import pyplot as plt
 
 # This is where the preliminary calo analysis code goes. Some representative output files from the simulation, including GDML angle data from photon impacts in the calo
 # should be placed in this folder. The calo data is then analyzed via various methods below.
@@ -17,7 +19,7 @@ def convert_to_spherical(coords):
 
 def euler_to_thetaPhi(euler, degrees=True):
     '''
-        Convert the Euler angles in the GDML file into a (theta,phi) pair for ploting
+        Convert the Euler angles in the GDML file into a (theta,phi) pair for plotting
     '''
     from scipy.spatial.transform import Rotation as Rot
     roti = Rot.from_euler("xyz", euler, degrees=degrees)
@@ -31,13 +33,14 @@ def euler_to_thetaPhi(euler, degrees=True):
     # print(angles)
     return angles
 
-def get_angles_from_gdml(infile, key='<rotation name="sipm_enclosure_'):
+#TODO: Use RegEx to get IDs of sipms, can use ID to find edep branch from root file
+def get_angles_from_gdml(infile, key='<rotation name="HEX'):
     angles = {}
     with open(infile, 'r') as f:
-        for line in f:
+        for line in f: 
             if key in line:
                 # print(line)
-                sipm = int(line.split(key)[1].split("_")[0])
+                sipm = int(line.split("0_")[1].split("_")[0])
                 x = float(line.split('x="')[1].split('"')[0])
                 y = float(line.split('y="')[1].split('"')[0])
                 z = float(line.split('z="')[1].split('"')[0])
@@ -46,7 +49,30 @@ def get_angles_from_gdml(infile, key='<rotation name="sipm_enclosure_'):
     return angles
 
 
-#TODO
 # plot a 2d histogram of energy deposited in each SIPM "pixel" in the calorimeter.
 def calo_edep_plot():
+    #Open file
+    # PiEfile = r.TFile("pienux_out_stripped.root")
+    # PiEtree = PiEfile.Get("atar")
+    # # print([x.GetName() for x in tree.GetListOfBranches()])
+    # # print("\n")
+
+    # (max_Es_DIF, gap_times_DIF) = event_visualization(PiEtree, 0, False, False, 100)
+    # (max_Es_DAR, gap_times_DAR) = event_visualization(PiEtree, 1, False, False, 100)
+
+    # compare_max_edep(max_Es_DIF, max_Es_DAR, 20)
+    # compare_gap_times(gap_times_DIF, gap_times_DAR, 20)
+
+    angles = get_angles_from_gdml("calo_only_PEN.gdml")
+    angles_list = list(angles.values())
+    angles_thetaPhi = [euler_to_thetaPhi(angles_list[i]) for i in range(len(angles))]
+    print(len(angles_thetaPhi))
+    plt.hist2d([ang[0] for ang in angles_thetaPhi], [ang[1] for ang in angles_thetaPhi], bins = 50)
+    plt.xlabel("Theta (rad)")
+    plt.ylabel("Phi (rad)")
+    plt.title("Energy Deposited in Calorimeter SiPMs by Theta vs. Phi")
+    plt.colorbar(orientation="vertical")
+    plt.show()
     return
+
+calo_edep_plot()
